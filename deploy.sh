885b4547da173b1852e3544500a8d61c85fb38ad
Jan Tuomi <jan@jantuomi.fi>
2024-07-14 10:36:45 +0300

    Use --progress plain in docker build

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 IMAGE=registry.jan.systems/jan-systems-2024:latest
 
 # Build
-docker build --platform linux/amd64 -t "$IMAGE" .
+docker build --progress plain --platform linux/amd64 -t "$IMAGE" .
 docker push "$IMAGE"
 
 # Deploy
